a8168824ad964abde21e5718623e072c01b6750e
grafmar <grafmar@FAST>
2019-01-28 22:59:05 +0100

    Update About Tab and set Version to V0.6.0.0

--- LyncPresenceBridge/AboutForm.Designer.cs
+++ LyncPresenceBridge/AboutForm.Designer.cs
@@ -30,6 +30,8 @@
         {
 			this.buttonAboutOK = new System.Windows.Forms.Button();
 			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// buttonAboutOK
@@ -45,13 +47,33 @@
 			// linkLabel1
 			// 
 			this.linkLabel1.AutoSize = true;
-			this.linkLabel1.Location = new System.Drawing.Point(12, 9);
+			this.linkLabel1.Location = new System.Drawing.Point(12, 22);
 			this.linkLabel1.Name = "linkLabel1";
 			this.linkLabel1.Size = new System.Drawing.Size(88, 13);
 			this.linkLabel1.TabIndex = 1;
 			this.linkLabel1.TabStop = true;
 			this.linkLabel1.Text = "GitHub repository";
 			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(193, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "LyncPresenceBridge is available under:";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 44);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(120, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "This is version: V?.?.?.?";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
 			// 
 			// AboutForm
 			// 
@@ -60,6 +82,8 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
 			this.ClientSize = new System.Drawing.Size(304, 104);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.linkLabel1);
 			this.Controls.Add(this.buttonAboutOK);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -69,6 +93,7 @@
 			this.ShowIcon = false;
 			this.ShowInTaskbar = false;
 			this.Text = "About LyncBlinker";
+			this.Load += new System.EventHandler(this.AboutForm_Load);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -78,5 +103,7 @@
 
         private System.Windows.Forms.Button buttonAboutOK;
         private System.Windows.Forms.LinkLabel linkLabel1;
-    }
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
 }
--- LyncPresenceBridge/AboutForm.cs
+++ LyncPresenceBridge/AboutForm.cs
@@ -19,5 +19,19 @@ namespace LyncPresenceBridge
         {
             System.Diagnostics.Process.Start("https://github.com/grafmar/Lync-presence-bridge");
         }
-    }
+
+		private void AboutForm_Load(object sender, EventArgs e)
+		{
+			label2.Text = "This is version: V" + Application.ProductVersion;
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
